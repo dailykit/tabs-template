cf65b7abf0db3116059160e6e03225fd1e031afa
Praveen Bisht <prvnbist@gmail.com>
2020-09-13 20:08:49 +0530

    feat: handle main overflow

--- src/styled.js
+++ src/styled.js
@@ -1,8 +1,12 @@
 import styled from "@emotion/styled";
 
 export const StyledWrapper = styled.div`
-  display: grid;
-  height: 100vh;
-  grid-template-columns: 1fr;
-  grid-template-rows: 40px 1fr;
+   display: grid;
+   height: 100vh;
+   grid-template-columns: 1fr;
+   grid-template-rows: 40px 1fr;
+   main {
+      overflow-y: auto;
+      height: calc(100vh - 40px);
+   }
 `;
